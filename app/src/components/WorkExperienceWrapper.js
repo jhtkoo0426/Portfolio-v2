3dfdc5ef0c29e2f68e34e1e9dd0365f8a5b3c8ff
jhtkoo0426 <jhtkoo@gmail.com>
2022-11-26 22:02:01 +0000

    Updated work experience component & css

--- app/src/components/WorkExperienceWrapper.js
+++ app/src/components/WorkExperienceWrapper.js
@@ -1,30 +1,40 @@
+import Anchor from './Anchor';
+import { Link } from 'react-router-dom';
 import TechStack from './TechStack';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 
 
 // React.js component for work experience descriptions.
-const WorkExperienceWrapper = ({ image, vid, company_name, duration, role, children, tech_stack}) => {
+const WorkExperienceWrapper = ({ image, vid, title, role, allow_explore, explore_link, tech_stack, children }) => {
     return (
         <div className="exp-wrapper">
-            <img className='exp-image' src={image} alt=""></img>
+            {
+                image != null &&
+                <img className='exp-image' src={image} alt=""></img>
+            }
             <div className="exp-content">
-                <h1 className="exp-content-title">{company_name}</h1>
-                {
-                    duration != null &&
-                    <span className='exp-duration'>
-                        {duration}
-                    </span>
-                }
+                <h1 className="exp-content-title">{title}</h1>
                 <p className='exp-role'>{role}</p>
                 <br></br>
                 {children}
-                <TechStack tags={tech_stack}></TechStack>
+                <br></br>
+                {
+                    tech_stack != null &&
+                    <TechStack tags={tech_stack}></TechStack>
+                }
+                <br></br><br></br>
+                {
+                    allow_explore == true &&
+                    <span>
+                        <Link className='exp-link' to={explore_link}>EXPLORE</Link>
+                        <svg className='arrow' width="45" height="11" viewBox="0 0 45 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M39.6364 9.98889L44 5.98889M44 5.98889L39.6364 1.98889M44 5.98889H1" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg>
+                    </span>
+                }
             </div>
             {
                 vid != null &&
-                <video autoPlay loop muted>
+                <video className='exp-video' autoPlay loop muted>
                     <source src={vid} type="video/mp4"></source>
                 </video>
             }
