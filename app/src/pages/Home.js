de347669d3c39148b6de3c9382053e7f3efb9981
jhtkoo0426 <jhtkoo@gmail.com>
2023-09-13 20:31:14 +0100

    Completed caset study 1 info

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -2,9 +2,9 @@ import Anchor from '../components/Anchor';
 import HugeTitle from '../components/HugeTitle';
 import TwoColContainer from '../components/TwoColumnContainer';
 import CaseStudyWrapper from '../components/CaseStudyWrapper';
+import dissertation_demo from "../videos/dissertation-demo.mp4";
 import l4s_demo from "../videos/l4s-landing-demo.mp4";
 import epidemic_demo from "../videos/epidemic-demo.mp4";
-
 
 const Home = () => {
     return (
@@ -38,15 +38,14 @@ const Home = () => {
                 <CaseStudyWrapper
                     id={"case-study2"}
                     project_id={1}
+                    vid={dissertation_demo}
                     title={"Robust Robotic Grapsing Utilising Touch Sensing"}
                     organisation={"Statistical Machine Learning Group UCL"}
-                    url={"https://github.com/jhtkoo0426/UCL-FYP"}
+                    url={"https://github.com/jhtkoo0426/UCL-FYP/blob/master/docs/Project%20Report/fyp_project_report.pdf"}
                     url_type={"external"}>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>A study on developing robust machine learning-guided systems for robotic grasping, conducted with the <Anchor href={"https://www.sml-group.cc"} inline={true}>Statistical 
+                    Machine Learning Group @ UCL</Anchor>. Addresses common training considerations, such as balancing the trade-off between dataset size 
+                    and accuracy.</p>
                 </CaseStudyWrapper>
             </TwoColContainer>
 
