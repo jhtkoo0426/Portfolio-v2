0474c885519e6418677272e16c23288306d5b92f
jhtkoo0426 <jhtkoo@gmail.com>
2023-09-20 23:35:59 +0100

    Reorganised momentum scrolling code

--- app/src/pages/Layout.js
+++ app/src/pages/Layout.js
@@ -12,24 +12,17 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const Layout = () => {
     var page_location = useLocation();
 
+    // Momentum scrolling
     const main = document.getElementById('app-container');
+    let sx = 0, sy = 0;   // For scroll positions
+    let dx = sx, dy = sy; // For container positions
 
-    let sx = 0, // For scroll positions
-        sy = 0;
-    let dx = sx, // For container positions
-        dy = sy;
-
-    
-    // Bind a scroll function
-    window.addEventListener('scroll', easeScroll);
-    
+    window.addEventListener('scroll', easeScroll);    // Bind a scroll function
     
     function easeScroll() {
-      
       sx = window.pageXOffset;
       sy = window.pageYOffset;
     }
-    
     
     window.requestAnimationFrame(render);
     
@@ -40,81 +33,75 @@ const Layout = () => {
       
       dx = Math.floor(dx * 100) / 100;
       dy = Math.floor(dy * 100) / 100;
-      
-      
+    
       main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
-      
-      
-      
       window.requestAnimationFrame(render);
     }
     
-    function li(a, b, n) {
-      return (1 - n) * a + n * b;
-        }
+    function li(a, b, n) { return (1 - n) * a + n * b; }
 
     return (
-        <div className="app-container" id="app-container">
-          {/* Top navigation bar */}
-          <div className="empty-container"></div>
-          <nav className="topnav-container">
-            <div className="home-button">
-              <Link to={"/"}>
-                <p className="j">j.</p>
-                {/* Changes the displayed page name depending on which page the user is on.
-                    Note: The nested ternary is for dealing with paths ending with a '/' character. */}
-                <p className="job-title">
-                  { page_location.pathname === "/"
-                    ? "data analyst & ML engineer"
-                    : page_location.pathname.endsWith("/")
-                      ? page_location.pathname.substring(0, page_location.pathname.length-1)
-                      : page_location.pathname
-                  }
-                </p>
-              </Link>
-            </div>
-            <div className="topnav-menu">
-              <Link className="anchor underline" to={"/about"}>about</Link>
-              <HashLink smooth className="anchor underline" to={'/about/#playground'}>projects</HashLink>
-              <Anchor inline={false} underline={true} href={"#contacts"} target={false}>contact</Anchor>
-            </div>
-          </nav>
+      <div className="app-container" id="app-container">
+        {/* Top navigation bar */}
+        <div className="empty-container"></div>
+        <nav className="topnav-container">
+          <div className="home-button">
+            <Link to={"/"}>
+              <p className="j">j.</p>
+              {/* Changes the displayed page name depending on which page the user is on.
+                  Note: The nested ternary is for dealing with paths ending with a '/' character. */}
+              <p className="job-title">
+                { page_location.pathname === "/"
+                  ? "data analyst & ML engineer"
+                  : page_location.pathname.endsWith("/")
+                    ? page_location.pathname.substring(0, page_location.pathname.length-1)
+                    : page_location.pathname
+                }
+              </p>
+            </Link>
+          </div>
+          <div className="topnav-menu">
+            <Link className="anchor underline" to={"/about"}>about</Link>
+            <HashLink smooth className="anchor underline" to={'/about/#playground'}>projects</HashLink>
+            <Anchor inline={false} underline={true} href={"#contacts"} target={false}>contact</Anchor>
+          </div>
+        </nav>
 
-          {/* Side navigation bar */}
-          <nav className="sidenav-container">
-            <ul>
-              <li>
-                <Anchor target={true} underline={true} href={"https://www.linkedin.com/in/koo-justin/"}>
-                  LinkedIn
-                </Anchor>
-              </li>
-              <li>
-                <Anchor target={true} underline={true} href={"https://github.com/jhtkoo0426"}>
-                  GitHub
-                </Anchor>
-              </li>
-            </ul>
-          </nav>
+        {/* Side navigation bar */}
+        <nav className="sidenav-container">
+          <ul>
+            <li>
+              <Anchor target={true} underline={true} href={"https://www.linkedin.com/in/koo-justin/"}>
+                LinkedIn
+              </Anchor>
+            </li>
+            <li>
+              <Anchor target={true} underline={true} href={"https://github.com/jhtkoo0426"}>
+                GitHub
+              </Anchor>
+            </li>
+          </ul>
+        </nav>
 
-          {/* Container for the main content of the portfolio */}
-          <Outlet/>
-          
-          {/* Bottom navigation bar */}
-          <nav className="botnav-container" id="contacts">
-            <div className="contacts-container">
-              <div>
-                <p>Still curious about how to bring those imaginative thoughts to life?</p>
-                <Title size={"small"}>let's get in touch!</Title>
-                <p>or you can find me via <Anchor target={true} inline={true} underline={false} href={"https://www.linkedin.com/in/koo-justin/"}>LinkedIn</Anchor>!</p>
-              </div>
-              <div className="blob2">
-                <Anchor className="blob2" href={"mailto:jhtkbusiness@gmail.com"} target={true} underline={false}>
-                  <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
-                </Anchor>
-              </div>
+        {/* Container for the main content of the portfolio */}
+        <Outlet/>
+        
+        {/* Bottom navigation bar */}
+        <nav className="botnav-container" id="contacts">
+          <div className="contacts-container">
+            <div>
+              <p>Still curious about how to bring those imaginative thoughts to life?</p>
+              <Title size={"small"}>let's get in touch!</Title>
+              <p>or you can find me via <Anchor target={true} inline={true} underline={false} href={"https://www.linkedin.com/in/koo-justin/"}>LinkedIn</Anchor>!</p>
             </div>
-          </nav>
-        </div>
+            <div className="blob2">
+              <Anchor className="blob2" href={"mailto:jhtkbusiness@gmail.com"} target={true} underline={false}>
+                <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+              </Anchor>
+            </div>
+          </div>
+        </nav>
+      </div>
     );
 };
 
